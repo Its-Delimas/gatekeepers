fd04a615777fda8fc6a2463744c622fc83d96dc6
Its-Delimas <spencerdelimas@gmail.com>
2026-08-11 22:10:39 +0300

    feat: pgx connection pool setup and wired into main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/Its-Delimas/gatekeepers/internals/config"
+	"github.com/Its-Delimas/gatekeepers/internals/db"
 )
 
 func main() {
@@ -11,5 +13,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to load config: %v", err)
 	}
-	log.Printf("starting server on port %s in %s mode", cfg.Port, cfg.Environment)
+
+	ctx := context.Background()
+	pool, err := db.NewPool(ctx, cfg.DatabaseURL)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+	defer pool.Close()
+
+	log.Printf("connected to database, starting server on port %s in %s mode", cfg.Port, cfg.Environment)
 }
