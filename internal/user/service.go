7578dc3010593dc0c366be6806e17a113364faf2
Its-Delimas <spencerdelimas@gmail.com>
2026-08-14 14:34:16 +0300

    feat: added  login service

--- internal/user/service.go
+++ internal/user/service.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Its-Delimas/gatekeepers/internal/db/sqlc"
 	"github.com/jackc/pgx/v5/pgconn"
 	"golang.org/x/crypto/bcrypt"
+	"github.com/google/uuid"
 )
 
 var ErrEmailTaken = errors.New("email is already registered")
@@ -38,4 +39,18 @@ func (s *Service) Register(ctx context.Context, email, password string) (sqlc.Us
 		return sqlc.User{}, fmt.Errorf("failed to create user: %w", err)
 	}
 	return user, nil
+}
+
+var ErrInvalidCredentials = errors.New("invalid email or password")
+
+func (s *Service) Login(ctx context.Context,email,password string)(sqlc.User, error){
+	u, err := s.queries.GetUserByEmail(ctx,email)
+	if err != nil {
+		return sqlc.User{}, ErrInvalidCredentials
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash),[]byte(password)); err != nil {
+		return sqlc.User{}, ErrInvalidCredentials
+	}
+	return u, nil
 }
